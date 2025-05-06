8ee153aab196a61ca297cb1cc7a6dd6e50aa499c
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-06 13:03:48 +0300

    palindrom controlled

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,27 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221));  // true
+        System.out.println(isPalindrome(707));    // true
+        System.out.println(isPalindrome(11212));  // false
     }
+
+    // 1. Palindrom Sayı Kontrolü
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        int original = number;
+        int reversed = 0;
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+            reversed = reversed * 10 + lastDigit;
+            number /= 10;
+        }
+
+        return original == reversed;
+    }
+
+
+
+
 }
