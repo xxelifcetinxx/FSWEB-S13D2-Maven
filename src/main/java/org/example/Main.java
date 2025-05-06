e1c688d935c162078cbf16f316d7647a6f1a788b
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-06 13:07:37 +0300

    PerfectNumber controlled

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,13 @@ public class Main {
         System.out.println(isPalindrome(-1221));  // true
         System.out.println(isPalindrome(707));    // true
         System.out.println(isPalindrome(11212));  // false
+
+        System.out.println(isPerfectNumber(6));   // true
+        System.out.println(isPerfectNumber(28));  // true
+        System.out.println(isPerfectNumber(5));   // false
+        System.out.println(isPerfectNumber(-1));  // false
+
+
     }
 
     // 1. Palindrom Sayı Kontrolü
@@ -20,6 +27,20 @@ public class Main {
         }
 
         return original == reversed;
+    }
+
+    // 2. Mükemmel Sayı Kontrolü
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) return false;
+
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == number;
     }
 
 
